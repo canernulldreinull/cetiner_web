492881c2c44a67e6fc145ab0b7f555f89c54fcf1
Caner Cetiner <ccanerr936590@gmail.com>
2026-06-13 20:09:19 +0300

    Mobil düzenlemeler

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-// NAV scroll effect
+if ("scrollRestoration" in history) {
+  history.scrollRestoration = "manual";
+}
+
+window.addEventListener("load", () => {
+  window.scrollTo(0, 0);
+});
 const navbar = document.getElementById('navbar');
 
 window.addEventListener('scroll', () => {
