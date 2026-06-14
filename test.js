31272e14976909a388dd52d0fff1c4b03325fb97
Caner Cetiner <ccanerr936590@gmail.com>
2026-06-14 04:31:12 +0300

    Remove test js file

--- test.js
+++
@@ -1,7 +0,0 @@
-const navbar = document.getElementById("navbar");
-
-if (navbar) {
-  window.addEventListener("scroll", () => {
-    navbar.classList.toggle("scrolled", window.scrollY > 50);
-  });
-}
